992a5a68cbadb4326a921b7774c59b575ba8f5fb
jdeora <jdeora@stevens.edu>
2023-02-10 21:39:36 -0500

    add triangle code

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,5 +1,5 @@
 import unittest
-from triangle import classifyTriangle
+from triangle import classifytriangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -8,61 +8,61 @@ from triangle import classifyTriangle
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
     def test_equilateral1(self):
-        self.assertEqual(classifyTriangle(3, 3, 3), 'Equilateral')
+        self.assertEqual(classifytriangle(3, 3, 3), 'Equilateral')
 
-    def testRightTriangleA(self):
-        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+    def testRighttriangleA(self):
+        self.assertEqual(classifytriangle(3, 4, 5), 'Right')
 
-    def testRightTriangleB(self):
-        self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5,3,4 is a Right triangle')
+    def testRighttriangleB(self):
+        self.assertEqual(classifytriangle(5, 3, 4), 'Right')
 
-    def testEquilateralTriangles(self):
-        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
+    def testEquilateraltriangles(self):
+        self.assertEqual(classifytriangle(1, 1, 1), 'Equilateral')
 
-    def test_Isoceles1(self):
-        self.assertEqual(classifyTriangle(2, 2, 3), 'Isoceles')
+    def test_Isosceles1(self):
+        self.assertEqual(classifytriangle(2, 3, 3), 'Isosceles')
 
-    def test_Isoceles2(self):
-        self.assertEqual(classifyTriangle(2, 3, 2), 'Isoceles')
+    def test_Isosceles2(self):
+        self.assertEqual(classifytriangle(2, 3, 2), 'Isosceles')
 
-    def test_Isoceles3(self):
-        self.assertEqual(classifyTriangle(3, 2, 2), 'Isoceles')
+    def test_Isosceles3(self):
+        self.assertEqual(classifytriangle(3, 2, 2), 'Isosceles')
 
     def test_Scalene(self):
-        self.assertEqual(classifyTriangle(3, 4, 5), 'Scalene')
+        self.assertEqual(classifytriangle(3, 6, 5), 'Scalene')
 
     def test_Right(self):
-        self.assertEqual(classifyTriangle(3, 4, 5), 'Right')
+        self.assertEqual(classifytriangle(3, 4, 5), 'Right')
 
     def test_Not_a_triangle1(self):
-        self.assertEqual(classifyTriangle(2, 2, 5), 'NotATriangle')
+        self.assertEqual(classifytriangle(2, 2, 5), 'NotATriangle')
 
     def test_Not_a_triangle2(self):
-        self.assertEqual(classifyTriangle(2, 5, 2), 'NotATriangle')
+        self.assertEqual(classifytriangle(2, 5, 2), 'NotATriangle')
 
     def test_Not_a_triangle3(self):
-        self.assertEqual(classifyTriangle(5, 2, 2), 'NotATriangle')
+        self.assertEqual(classifytriangle(5, 2, 2), 'NotATriangle')
 
     def test_Invalid_input1(self):
-        self.assertEqual(classifyTriangle(-2, 2, 2), 'InvalidInput')
+        self.assertEqual(classifytriangle(-2, 2, 2), 'InvalidInput')
 
     def test_Invalid_input2(self):
-        self.assertEqual(classifyTriangle(2, -2, 2), 'InvalidInput')
+        self.assertEqual(classifytriangle(2, -2, 2), 'InvalidInput')
 
     def test_Invalid_input3(self):
-        self.assertEqual(classifyTriangle(2, 2, -2), 'InvalidInput')
+        self.assertEqual(classifytriangle(2, 2, -2), 'InvalidInput')
 
     def test_Invalid_input4(self):
-        self.assertEqual(classifyTriangle(2, 2, 201), 'InvalidInput')
+        self.assertEqual(classifytriangle(2, 2, 201), 'InvalidInput')
 
     def test_Invalid_input5(self):
-        self.assertEqual(classifyTriangle(1, 2, 2), 'InvalidInput')
+        self.assertEqual(classifytriangle(-1, 2, 2), 'InvalidInput')
 
     def test_Invalid_input6(self):
-        self.assertEqual(classifyTriangle(2, 2, 2), 'InvalidInput')
+        self.assertEqual(classifytriangle(2, 2, -4), 'InvalidInput')
 
     def test_Invalid_input7(self):
-        self.assertEqual(classifyTriangle(2, 2, 5), 'InvalidInput')
+        self.assertEqual(classifytriangle(-2, 2, 5), 'InvalidInput')
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
