9bf14eca003afe6a4a6f57368fc8ab7f9d5ffb70
jdeora <jdeora@stevens.edu>
2023-03-10 17:52:56 -0500

    add triangle code

--- TestTriangle.py
+++ TestTriangle.py
@@ -8,7 +8,7 @@ from triangle import classify_triangle
 class TestTriangle(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
     def test_equilateral1(self):
-        self.assertEqual(classify_triangle(3, 3, 3), 'Equilateral')
+        self.assertEqual(classify_triangle([3, 3, 3]), 'Equilateral')
 
     def testRighttriangleA(self):
         self.assertEqual(classify_triangle([3, 4, 5]), 'Right')
@@ -35,31 +35,31 @@ class TestTriangle(unittest.TestCase):
         self.assertEqual(classify_triangle([3, 4, 5]), 'Right')
 
     def test_Not_a_triangle1(self):
-        self.assertEqual(classify_triangle([2, 2, 5]), 'NotATriangle')
+        self.assertEqual(classify_triangle([2, 2, 5]), 'Not_A_Triangle')
 
     def test_Not_a_triangle2(self):
-        self.assertEqual(classify_triangle([2, 5, 2]), 'NotATriangle')
+        self.assertEqual(classify_triangle([2, 5, 2]), 'Not_A_Triangle')
 
     def test_Not_a_triangle3(self):
-        self.assertEqual(classify_triangle([5, 2, 2]), 'NotATriangle')
+        self.assertEqual(classify_triangle([5, 2, 2]), 'Not_A_Triangle')
 
     def test_Invalid_input1(self):
-        self.assertEqual(classify_triangle([-2, 2, 2]), 'InvalidInput')
+        self.assertEqual(classify_triangle([-2, 2, 2]), 'Invalid_Input')
 
     def test_Invalid_input2(self):
-        self.assertEqual(classify_triangle([2, -2, 2]), 'InvalidInput')
+        self.assertEqual(classify_triangle([2, -2, 2]), 'Invalid_Input')
 
     def test_Invalid_input3(self):
-        self.assertEqual(classify_triangle([2, 2, -2]), 'InvalidInput')
+        self.assertEqual(classify_triangle([2, 2, -2]), 'Invalid_Input')
 
     def test_Invalid_input4(self):
-        self.assertEqual(classify_triangle([2, 2, 201]), 'InvalidInput')
+        self.assertEqual(classify_triangle([2, 2, 201]), 'Invalid_Input')
 
     def test_Invalid_input5(self):
-        self.assertEqual(classify_triangle([-1, 2, 2]), 'InvalidInput')
+        self.assertEqual(classify_triangle([-1, 2, 2]), 'Invalid_Input')
 
     def test_Invalid_input6(self):
-        self.assertEqual(classify_triangle([2, 2, -4]), 'InvalidInput')
+        self.assertEqual(classify_triangle([2, 2, -4]), 'Invalid_Input')
 
 if __name__ == '__main__':
     print('Running unit tests')
